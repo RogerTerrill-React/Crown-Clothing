648ed904ec4b835266c0b9138037c7218210a67e
Roger Dodger <lpaddikt@gmail.com>
2021-01-31 15:24:12 -0800

    Homepage and Directory components finished

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/Directory';
+import './Homepage.scss';
+
+const Hompage = () => {
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
+};
+
+export default Hompage;
