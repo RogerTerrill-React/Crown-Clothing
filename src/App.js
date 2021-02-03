39cebc32bec63b5efca01d83e34778d548d6b4b6
Roger Dodger <lpaddikt@gmail.com>
2021-02-02 20:01:06 -0800

    Finished Shop Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/Homepage';
-
-function Hats() {
-  return (
-    <div>
-      HATS
-    </div>
-  )
-}
+import Homepage from './pages/homepage';
+import ShopPage from './pages/shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/hats' component={Hats} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/shop/ShopPage.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import SHOP_DATA from '../../helpers/shopData.json';
+import CollectionPreview from '../../components/CollectionPreview';
+
+const ShopPage = () => {
+  const INTIAL_STATE = SHOP_DATA;
+
+  const [collections, setCollections] = useState(INTIAL_STATE);
+  return (
+    <div className='shop-page'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+export default ShopPage;
