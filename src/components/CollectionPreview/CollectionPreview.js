496597d4afa4b56a15012d18b0e585a8967fabf7
Roger Dodger <lpaddikt@gmail.com>
2021-02-03 20:26:13 -0800

    Finished Collection Item

--- src/components/CollectionPreview/CollectionPreview.js
+++ src/components/CollectionPreview/CollectionPreview.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import CollectionItem from '../CollectionItem';
 
 import './CollectionPreview.scss';
 
 const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
-          ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className='collection-preview'>
+			<h1 className='title'>{title.toUpperCase()}</h1>
+			<div className='preview'>
+				{items
+					.filter((item, idx) => idx < 4)
+					.map(({ id, ...otherItemProps }) => (
+						<CollectionItem key={id} {...otherItemProps} />
+					))}
+			</div>
+		</div>
+	);
 };
 
 export default CollectionPreview;
--- src/pages/shop/ShopPage.js
+++ src/pages/shop/ShopPage.js
@@ -3,16 +3,16 @@ import SHOP_DATA from '../../helpers/shopData.json';
 import CollectionPreview from '../../components/CollectionPreview';
 
 const ShopPage = () => {
-  const INTIAL_STATE = SHOP_DATA;
+	const INTIAL_STATE = SHOP_DATA;
 
-  const [collections, setCollections] = useState(INTIAL_STATE);
-  return (
-    <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
+	const [collections, setCollections] = useState(INTIAL_STATE);
+	return (
+		<div className='shop-page'>
+			{collections.map(({ id, ...otherCollectionProps }) => (
+				<CollectionPreview key={id} {...otherCollectionProps} />
+			))}
+		</div>
+	);
 };
 
 export default ShopPage;
