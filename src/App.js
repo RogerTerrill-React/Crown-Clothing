cf28f8dc6cbdd9ac5aab05d7c870dda891a7acdd
Roger Dodger <lpaddikt@gmail.com>
2021-02-21 21:45:29 -0800

    Routing conditioned on user logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 import Homepage from './pages/homepage';
@@ -8,25 +8,33 @@ import SignIn from './pages/signin';
 import Header from './components/Header';
 import { setCurrentUser } from './redux/user/userActions';
 
-function App({setCurrentUser}) {
+function App({ setCurrentUser, currentUser }) {
 	useEffect(() => {
-		setCurrentUser('RogerT');
-	})
+		setCurrentUser('Roger');
+	});
 	return (
 		<div>
 			<Header />
 			<Switch>
 				<Route exact path='/' component={Homepage} />
-				<Route exact path='/shop' component={ShopPage} />
-				<Route exact path='/signin' component={SignIn} />
+				<Route path='/shop' component={ShopPage} />
+				<Route exact path='/signin'>
+					{currentUser ? <Redirect to='/' /> : <SignIn />}
+				</Route>
 			</Switch>
 		</div>
 	);
 }
+
+const mapStateToProps = ({ user }) => {
+	return {
+		currentUser: user.currentUser,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 	};
 };
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
